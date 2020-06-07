81c88201810e50a556606b61ac5385669bf98847
gtorres7 <gtorres6@ucol.mx>
2020-06-06 21:36:20 -0500

    random simulation

---
+++ app.js
@@ -0,0 +1,6 @@
+setInterval(function(){   
+    humedad.textContent=(Math.floor((Math.random()*100)+1)); 
+    temperatura.textContent=(Math.floor((Math.random()*100)+1));
+    co2.textContent=(Math.floor((Math.random()*100)+1));
+ }, 1000);
+
